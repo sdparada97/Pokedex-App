82a751296eda7fdf8b4a0bbffbd983419c9ee70b
Sergio <sdparada97@gmail.com>
2023-08-12 05:13:55 -0500

    🆕 feature: Adding logging

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,7 +1,10 @@
+import logging
+
 from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def poke_app():
+    logging.debug("debug log")
     return render_template('pokedex.html')
