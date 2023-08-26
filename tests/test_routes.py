01f90a01318904f808eddae7787e7de860349e86
Sergio <sdparada97@gmail.com>
2023-08-26 12:47:00 -0500

    🆕 feature: Adding tests and fix the gitignore

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,5 +1,15 @@
 import pytest
+import aiohttp
+from app.routes.main import get_all_pokemons
 
 
-class test_routes_main():
-    pass
+@pytest.mark.asyncio
+async def test_get_all_pokemons():
+    pokemon_data = await get_all_pokemons(1)
+
+    assert pokemon_data['name'] == 'bulbasaur'
+    assert 'abilities' in pokemon_data
+
+    with pytest.raises(aiohttp.ClientError):
+        await get_all_pokemons(10000)
+
